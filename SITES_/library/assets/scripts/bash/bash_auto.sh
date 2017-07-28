08d920fe1c8356b8361a5f944ce6975bd139014a
jahcoozi <rajahcooper360@outlook.com>
2017-07-28 05:05:43 -0500

    removed all

--- SITES_/library/assets/scripts/bash/bash_auto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### export path ###
-#export PATH=$PATH:/mnt/cUsers/raja/Desktop/mygit/jahcoozi/.bash_auto.sh
-#echo "$PATH"
-#to fire internal functions use {{{{   source this; func         }}}}
-tzzz(){
-echo $1
-}
